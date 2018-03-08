ae5018e196a422f5100f272a5c1d327269cf1517
Adam Woodliffe <34106023+Woody91@users.noreply.github.com>
2018-03-08 16:55:04 +0000

    Added 2 Genra Textboxes
    Top half of Jukebox

--- Jukebox Assignment/Form1.Designer.cs
+++ Jukebox Assignment/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GenreTitleBox = new System.Windows.Forms.TextBox();
+            this.GenreListBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // GenreTitleBox
+            // 
+            this.GenreTitleBox.Location = new System.Drawing.Point(331, 213);
+            this.GenreTitleBox.Name = "GenreTitleBox";
+            this.GenreTitleBox.Size = new System.Drawing.Size(100, 20);
+            this.GenreTitleBox.TabIndex = 0;
+            // 
+            // GenreListBox
+            // 
+            this.GenreListBox.Location = new System.Drawing.Point(331, 240);
+            this.GenreListBox.Name = "GenreListBox";
+            this.GenreListBox.Size = new System.Drawing.Size(100, 20);
+            this.GenreListBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(762, 720);
+            this.Controls.Add(this.GenreListBox);
+            this.Controls.Add(this.GenreTitleBox);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox GenreTitleBox;
+        private System.Windows.Forms.TextBox GenreListBox;
     }
 }
 
