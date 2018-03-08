1f4d8307061c265474d068067ed6f8a07063b64e
Adam Woodliffe <34106023+Woody91@users.noreply.github.com>
2018-03-08 17:14:16 +0000

    Most Components Added to GUI
    Textboxes and Listboxes etc.

--- Jukebox Assignment/Form1.Designer.cs
+++ Jukebox Assignment/Form1.Designer.cs
@@ -30,7 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.GenreTitleBox = new System.Windows.Forms.TextBox();
-            this.GenreListBox = new System.Windows.Forms.TextBox();
+            this.GenreListBox = new System.Windows.Forms.ListBox();
+            this.PlaylistListBox = new System.Windows.Forms.ListBox();
+            this.PlayNowTextBox = new System.Windows.Forms.TextBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
             // 
             // GenreTitleBox
@@ -42,10 +46,37 @@
             // 
             // GenreListBox
             // 
-            this.GenreListBox.Location = new System.Drawing.Point(331, 240);
+            this.GenreListBox.FormattingEnabled = true;
+            this.GenreListBox.HorizontalScrollbar = true;
+            this.GenreListBox.Location = new System.Drawing.Point(312, 240);
             this.GenreListBox.Name = "GenreListBox";
-            this.GenreListBox.Size = new System.Drawing.Size(100, 20);
+            this.GenreListBox.ScrollAlwaysVisible = true;
+            this.GenreListBox.Size = new System.Drawing.Size(138, 95);
             this.GenreListBox.TabIndex = 1;
+            // 
+            // PlaylistListBox
+            // 
+            this.PlaylistListBox.FormattingEnabled = true;
+            this.PlaylistListBox.Location = new System.Drawing.Point(312, 386);
+            this.PlaylistListBox.Name = "PlaylistListBox";
+            this.PlaylistListBox.Size = new System.Drawing.Size(138, 134);
+            this.PlaylistListBox.TabIndex = 2;
+            // 
+            // PlayNowTextBox
+            // 
+            this.PlayNowTextBox.Location = new System.Drawing.Point(312, 342);
+            this.PlayNowTextBox.Name = "PlayNowTextBox";
+            this.PlayNowTextBox.Size = new System.Drawing.Size(138, 20);
+            this.PlayNowTextBox.TabIndex = 3;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(261, 152);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(241, 46);
+            this.axWindowsMediaPlayer1.TabIndex = 4;
             // 
             // Form1
             // 
@@ -54,12 +85,16 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(762, 720);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.PlayNowTextBox);
+            this.Controls.Add(this.PlaylistListBox);
             this.Controls.Add(this.GenreListBox);
             this.Controls.Add(this.GenreTitleBox);
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,7 +103,10 @@
         #endregion
 
         private System.Windows.Forms.TextBox GenreTitleBox;
-        private System.Windows.Forms.TextBox GenreListBox;
+        private System.Windows.Forms.ListBox GenreListBox;
+        private System.Windows.Forms.ListBox PlaylistListBox;
+        private System.Windows.Forms.TextBox PlayNowTextBox;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
     }
 }
 
